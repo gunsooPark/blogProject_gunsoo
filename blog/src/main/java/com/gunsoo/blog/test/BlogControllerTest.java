027ff1cd400f6bf2c88c137a633c5ac7aeb14f20
gunsooPark <gunsoo523@gmail.com>
2021-12-27 16:51:20 +0900

    6강수업  security미적용 완료

--- blog/src/main/java/com/gunsoo/blog/test/BlogControllerTest.java
+++ blog/src/main/java/com/gunsoo/blog/test/BlogControllerTest.java
@@ -1,5 +1,7 @@
 package com.gunsoo.blog.test;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 //특정 어노테이션이 붙어있는 클래스 파일들을 new해서(IoC) 스프링 컨테니어에 관리해준다.
 
 @RestController
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+
 public class BlogControllerTest {
 
+	
 	//
 	@GetMapping("/test/hello")
 	public String hello() {
