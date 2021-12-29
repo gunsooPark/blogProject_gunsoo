484a072f67a73b651ebb801f1dbfa49ecae702fa
gunsooPark <gunsoo523@gmail.com>
2021-12-29 11:55:47 +0900

    21강 테이블 생성 완료

--- blog/src/main/java/com/gunsoo/blog/model/Reply.java
+++ blog/src/main/java/com/gunsoo/blog/model/Reply.java
@@ -1,5 +1,54 @@
 package com.gunsoo.blog.model;
 
-public class Reply {
+import java.sql.Timestamp;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Reply {
+	
+	@Id //Primary Key
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //프로젝트에서 연겨된 DB의 넘버링 전략을 따라감. 
+	private int id;
+	
+	@Column(nullable = false, length = 200)
+	private String content;
+	
+	@ManyToOne //Many -> Reply, One -> Board
+	@JoinColumn(name = "boardId")
+	private Board board;
+	
+	@ManyToOne //Many -> Reply, One -> Board
+	@JoinColumn(name = "userId")
+	private User user;
+	
+	@CreationTimestamp
+	private Timestamp createDate;
+	
+	
 }
+
+
+
+
+
+
+
+
