05c0646f6bbf337a2e156fd5cdbfe592c3679411
gunsooPark <gunsoo523@gmail.com>
2022-01-04 14:17:58 +0900

    ㅇㅇㅇ

---
+++ blog/bin/main/static/js/user.js
@@ -0,0 +1,26 @@
+let index = {
+	init:function(){
+		$("#btn-save").on("click", ()=>{
+			this.save();
+		});	
+	},
+	
+	save:function(){
+		//alert("user의 save가 호출됬음");
+		let data = {
+			username : $("#username").val(),
+			password : $("#password").val(),
+			email : $("#email").val()
+		}
+		
+		//console.log(data);
+		
+		
+		$.ajax().done.fail(); //ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert요청을 할예정 
+		
+	}
+	
+}
+
+
+index.init();
